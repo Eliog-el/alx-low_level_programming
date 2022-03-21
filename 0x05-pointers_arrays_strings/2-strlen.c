d8f8996f0666a1cb3c74d891b07ecc8b364ae552
Eliog Olusola <elijahogunrinade@gmail.com>
2022-03-21 15:29:52 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strlen.c - returns the length of a string
+ * _strlen - returns the length of a string
  * @s: string to evaluate
  *
  * Retrun: the length of the string
